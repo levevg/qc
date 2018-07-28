59f68b8904c6fc1ffbde34e1d0a25d7874a55d12
levevg <unstutby@gmail.com>
2018-07-28 22:25:54 +0300

    auth

--- modules/front.class.php
+++ modules/front.class.php
@@ -7,6 +7,14 @@ var $no_install = true;
 function run(){
 	if ($this->action=='error404') error404();
 
+	if ($this->action=='') {
+        if ($_SERVER['PHP_AUTH_USER'] != AUTH_USER || $_SERVER['PHP_AUTH_PW'] != AUTH_PASS) {
+            header('WWW-Authenticate: Basic realm="index"');
+            header('HTTP/1.0 401 Unauthorized');
+            die('Пользователь - название чата, пароль - название команды');
+        }
+    }
+
     $out = array('page_title' => SETTINGS_SITE_TITLE);
 
     $this->template = 'index.tpl';
